a266bc46482c928a2542f4f4ed074cd168ab3cda
matiseekk-dot <matiseekk@gmail.com>
2026-04-16 18:02:11 +0200

    1.1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/babylog/',
+  build: {
+    // Ensure public dir files including hidden .well-known are copied
+    assetsDir: 'assets',
+  },
+  publicDir: 'public',
 })
